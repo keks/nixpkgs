e1ce41b36a31f01a583e28a17bd2cb2dd37b97e7
keks <keks@cryptoscope.co>
2018-10-02 15:19:07 +0200

    initial attempt. not working.

---
+++ pkgs/applications/altcoins/beam-mw.nix
@@ -0,0 +1,36 @@
+{ stdenv, fetchFromGitHub
+, openssl, boost, cmake, qt5 }:
+
+with stdenv.lib;
+stdenv.mkDerivation rec {
+
+  name = "beam-mw" + "-" + version;
+  version = "0.16.0";
+
+  src = fetchFromGitHub {
+    owner = "beam-mw";
+    repo = "beam";
+    rev = "6ce6a409c839ce7cebb0aa1a7cc1d1154458a07e";
+    sha256 = "0z94xp0f1ldly96crjlv5bwjbb5r32hzx9v8zi8hdk7syjp748bx";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ openssl boost qt5.full ];
+
+  cmakeFlags = [
+	"-DCMAKE_BUILD_TYPE=Release"
+  ];
+
+  configureFlags = [ "--with-boost-libdir=${boost.out}/lib" ];
+
+  meta = {
+    description = "BEAM Mimblewimble";
+    longDescription= ''
+		Some cryptocurrency.
+    '';
+    homepage = https://beam-mw.com/;
+    platforms = platforms.unix;
+    license = licenses.apache2;
+    #maintainers = with maintainers; [ offline AndersonTorres ];
+  };
+}
